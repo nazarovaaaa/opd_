5791114773222f97387d6800413a65319a2481a8
nazarovaaaa <artem.a.nazarov@yandex.ru>
2022-04-10 23:37:12 +0300

    add class for countdown

---
+++ Task_Project2/Program.cs
@@ -0,0 +1,16 @@
+﻿using Nazarovv2;
+
+var timer = new CountDown();
+
+var subscriberA = new Subscriber(timer, "Субскрубер А");
+var subscriberB = new Subscriber(timer, "Субскрубер Б");
+var subscriberC = new Subscriber(timer, "Субскрубер Ц");
+
+timer.Skip(1000, "вот вам событие");
+timer.Skip(1000, "вот вам второе событие");
+subscriberA.Ubsubscribe();
+timer.Skip(2000, "вот вам третье событие");
+subscriberB.Ubsubscribe();
+timer.Skip(1000, "вот вам четвертое событие");
+subscriberC.Ubsubscribe();
+timer.Skip(1000, "вот вам пятое событие");
